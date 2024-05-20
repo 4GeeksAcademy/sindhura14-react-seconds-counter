4576f4312201c97bc926e2de6e8f77b1b3521fba
sindhura14 <sindhu.kilaru14@gmail.com>
2024-05-20 22:31:15 +0000

    changes to start a seconds counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
 import React from "react";
+import { useState,useEffect } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [time, setTime] = useState(0);
+
+  React.useEffect(() => {
+    let interval = null;
+    interval = setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+
+  return (
+	<div className="timer">
+	<i className="fa-regular fa-clock"></i>
+	  <p className="digits">{Math.floor((time / 100000) % 10)}</p>
+	  <p className="digits">{Math.floor((time / 10000) % 10)}</p>
+	  <p className="digits">{Math.floor((time / 1000) % 10)}</p>
+	  <p className="digits">{Math.floor((time / 100) % 10)}</p>
+	  <p className="digits">{Math.floor((time / 10) % 10)}</p>
+	  <p className="digits">{Math.floor((time / 1) % 10)}</p>
+	</div>
+  );
 };
 
 export default Home;
